5665ae4e291beac17db3bd8fa6ddc30d9d88c1ae
NekoCarreraDesigns <denvermaas@gmail.com>
2023-07-28 15:11:35 -0600

    added Projects code to file

--- portfolio-site-four/src/App.js
+++ portfolio-site-four/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Projects from "./pages/Projects/Projects";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/projects' element={<Projects />}></Route>
       </Routes>
     </>
   );
