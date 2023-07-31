cff6e43dc1a953f1207a8f7b2dbe0e86913412eb
NekoCarreraDesigns <denvermaas@gmail.com>
2023-07-31 15:22:44 -0600

    added second game to projects page

--- portfolio-site-four/src/pages/Projects/Projects.js
+++ portfolio-site-four/src/pages/Projects/Projects.js
@@ -3,15 +3,43 @@ import "./Projects.css";
 const Projects = () => {
   return (
     <>
-      <div className='wrapper'>
+      <div className='header-container'>
         <h1 className='projects-header'>Fun Games</h1>
-        {/* <iframe
-          title='snake-game'
-          src='https://drive.google.com/file/d/1spgG5H_1atO80wIprotTQWSqWJf5YAYt/preview'
-          width='640'
-          height='480'></iframe> */}
-        <img src='Snake_Game.gif' alt='snake-game'></img>
-        <a href='/main.exe'>Play me!</a>
+        <div className='project-container'>
+          <div className='project-div'>
+            <h2 className='game-header'>Snake</h2>
+            <iframe
+              title='snake-game'
+              src='https://drive.google.com/file/d/1spgG5H_1atO80wIprotTQWSqWJf5YAYt/preview'
+              width='640'
+              height='480'></iframe>
+            {/* <img src='Snake_Game.gif' alt='snake-game'></img> */}
+
+            <p>
+              Snake Game made using Python, the game uses object oriented
+              programming to draw the board, as well as using functional
+              programming for the controls
+              <button className='play-me-button'>
+                <a href='/main.exe'>Play me!</a>
+              </button>
+            </p>
+          </div>
+          <div className='project-div'>
+            <h2 className='game-header'>Connect Four</h2>
+            <iframe
+              title='Connect-four'
+              src='https://drive.google.com/file/d/1tCl2AWszlp2nD5c4dBYqfU91tL24Nz_1/preview'
+              width='640'
+              height='480'></iframe>
+            <p>
+              Game was made using Python, Pygame, and Numpy. Uses functional
+              programming to draw, and provide the games functionality
+              <button>
+                <a href='/connect_four.exe'>Play me!</a>
+              </button>
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
