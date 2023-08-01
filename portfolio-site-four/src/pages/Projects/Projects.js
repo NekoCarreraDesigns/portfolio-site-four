9554640c295e6d89360766afeec9047d4fc363b6
NekoCarreraDesigns <denvermaas@gmail.com>
2023-08-01 14:42:57 -0600

    added tetris to games section and changed iframes to gifs

--- portfolio-site-four/src/pages/Projects/Projects.js
+++ portfolio-site-four/src/pages/Projects/Projects.js
@@ -8,12 +8,12 @@ const Projects = () => {
         <div className='project-container'>
           <div className='project-div'>
             <h2 className='game-header'>Snake</h2>
-            <iframe
+            {/* <iframe
               title='snake-game'
               src='https://drive.google.com/file/d/1spgG5H_1atO80wIprotTQWSqWJf5YAYt/preview'
               width='640'
-              height='480'></iframe>
-            {/* <img src='Snake_Game.gif' alt='snake-game'></img> */}
+              height='480'></iframe> */}
+            <img src='Snake_Game.gif' alt='snake-game'></img>
 
             <p>
               Snake Game made using Python, the game uses object oriented
@@ -26,11 +26,12 @@ const Projects = () => {
           </div>
           <div className='project-div'>
             <h2 className='game-header'>Connect Four</h2>
-            <iframe
+            {/* <iframe
               title='Connect-four'
               src='https://drive.google.com/file/d/1tCl2AWszlp2nD5c4dBYqfU91tL24Nz_1/preview'
               width='640'
-              height='480'></iframe>
+              height='480'></iframe> */}
+              <img src="Connect_Four.gif" alt="connect-four"></img>
             <p>
               Game was made using Python, Pygame, and Numpy. Uses functional
               programming to draw, and provide the games functionality
@@ -38,6 +39,14 @@ const Projects = () => {
                 <a href='/connect_four.exe'>Play me!</a>
               </button>
             </p>
+          </div>
+          <div className="project-div">
+            <h2 className="game-header">Tetris</h2>
+            <img src="Tetris.gif"></img>
+            <p>This game was made using Python and Pygame. It uses Object oriented programming to draw the board and game pieces and functional programming for the controls</p>
+            <button>
+                <a href='/tetris.exe'>Play me!</a>
+              </button>
           </div>
         </div>
       </div>
