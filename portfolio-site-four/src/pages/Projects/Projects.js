29a87beb6a6d114d3267e3bfd1f74c74a82c02ac
NekoCarreraDesigns <denvermaas@gmail.com>
2023-08-14 16:14:29 -0600

    added sites to Projects page

--- portfolio-site-four/src/pages/Projects/Projects.js
+++ portfolio-site-four/src/pages/Projects/Projects.js
@@ -4,6 +4,32 @@ const Projects = () => {
   return (
     <>
       <div className='header-container'>
+        <div className='header-container'>
+          <h1 className='projects-header'>Sites</h1>
+          <div className='project-container'>
+            <div className='project-div'>
+              <h2 className='site-header'>eFormative</h2>
+              <img src='eFormative.gif' alt='website'></img>
+              <p>
+                Website made with the MERN stack. MongoDB database to store user
+                information, and store objects that the user is selling. Express
+                routes to make the site CRUD capable, React frontend for the UI,
+                and Node to make it serverless. The site uses material UI for a
+                nice clean presentation
+              </p>
+              <a href='https://eformative.com'>Visit</a>
+            </div>
+            <div className='project-div'>
+              <h2 className='site-header'>The Antique Geek</h2>
+              <img src='The Antique Geek.gif' alt='website'></img>
+              <p>
+                Static website made using React, and react-router-dom
+                navigation. The site uses ES6 and some CSS for styling the page.
+              </p>
+              <a href='https://eformative.com'>Visit</a>
+            </div>
+          </div>
+        </div>
         <h1 className='projects-header'>Fun Games</h1>
         <div className='project-container'>
           <div className='project-div'>
@@ -13,7 +39,7 @@ const Projects = () => {
               src='https://drive.google.com/file/d/1spgG5H_1atO80wIprotTQWSqWJf5YAYt/preview'
               width='640'
               height='480'></iframe> */}
-            <img src='Snake_Game.gif' alt='snake-game'></img>
+            <img src='Snake_Game_1.gif' alt='snake-game'></img>
 
             <p>
               Snake Game made using Python, the game uses object oriented
@@ -31,7 +57,7 @@ const Projects = () => {
               src='https://drive.google.com/file/d/1tCl2AWszlp2nD5c4dBYqfU91tL24Nz_1/preview'
               width='640'
               height='480'></iframe> */}
-              <img src="Connect_Four.gif" alt="connect-four"></img>
+            <img src='Connect_Four_1.gif' alt='connect-four'></img>
             <p>
               Game was made using Python, Pygame, and Numpy. Uses functional
               programming to draw, and provide the games functionality
@@ -40,13 +66,17 @@ const Projects = () => {
               </button>
             </p>
           </div>
-          <div className="project-div">
-            <h2 className="game-header">Tetris</h2>
-            <img src="Tetris.gif"></img>
-            <p>This game was made using Python and Pygame. It uses Object oriented programming to draw the board and game pieces and functional programming for the controls</p>
+          <div className='project-div'>
+            <h2 className='game-header'>Tetris</h2>
+            <img src='Tetris_1.gif' alt='tetris'></img>
+            <p>
+              This game was made using Python and Pygame. It uses Object
+              oriented programming to draw the board and game pieces and
+              functional programming for the controls
+            </p>
             <button>
-                <a href='/tetris.exe'>Play me!</a>
-              </button>
+              <a href='/tetris.exe'>Play me!</a>
+            </button>
           </div>
         </div>
       </div>
