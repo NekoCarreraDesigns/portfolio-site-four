e00ed427deb1d74bc4e7296d3cc3d062bf43f533
NekoCarreraDesigns <denvermaas@gmail.com>
2023-08-14 16:35:18 -0600

    added contact info to page

---
+++ portfolio-site-four/src/pages/Contact/Contact.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <>
+      <h1 className='contact-header'>Contact</h1>
+      <p className='contact-paragraph'>
+        My name is Nicholas Maas, owner and Junior Developer at Neko Carrera
+        Designs. Please feel free to contact me
+      </p>
+      <div className='contact-list'>
+        <li>Phone: 303-931-0974</li>
+        <li>Email: denvermaas@gmail.com</li>
+        <li>LinkedIn: www.linkedin.com/in/nekocarrera1978</li>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
